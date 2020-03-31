52e9a97becfdfef5438b9d3be8c9be9646f4289c
yatki <mehmet@yatki.com>
2020-03-31 04:29:39 +0300

    Remove redundant localeName method

--- 01-hello-world-node/index.js
+++ 01-hello-world-node/index.js
@@ -9,14 +9,13 @@ const languages = { 1: "tr", 2: "en" };
 
 readline.question("1 - Türkçe, 2 - English: ", no => {
   const lang = languages[no] || "tr";
-
-  lisan.localeName(lang);
-
   const mainDict = require(`./.lisan_out/dictionaries/${lang}/main.js`);
+
   lisan.add(mainDict);
 
   console.log(t("hello.world"));
   console.log(t("node.version", { version: process.version }));
   console.log(t("done.message"));
+
   readline.close();
 });
