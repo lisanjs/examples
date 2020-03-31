def9914e0642c9ce9d6adc9479acca54d8c3a3b2
yatki <mehmet@yatki.com>
2020-03-31 04:25:50 +0300

    Add hello world example for node

---
+++ 01-hello-world-node/index.js
@@ -0,0 +1,22 @@
+const { lisan, t } = require("lisan");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const languages = { 1: "tr", 2: "en" };
+
+readline.question("1 - Türkçe, 2 - English: ", no => {
+  const lang = languages[no] || "tr";
+
+  lisan.localeName(lang);
+
+  const mainDict = require(`./.lisan_out/dictionaries/${lang}/main.js`);
+  lisan.add(mainDict);
+
+  console.log(t("hello.world"));
+  console.log(t("node.version", { version: process.version }));
+  console.log(t("done.message"));
+  readline.close();
+});
